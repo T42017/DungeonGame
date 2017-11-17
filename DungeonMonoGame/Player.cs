d760169d190ceff22a1150560b2768059b0cc3c4
LucasM <lucas.mikkelsen@lund.ntig.se>
2017-11-17 09:55:41 +0100

    Made a basic player.cs but it will probably need more to it later on!

---
+++ DungeonMonoGame/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DungeonMonoGame
+{
+    public class Player
+    {
+        public bool IsDead { get; set; }
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Attack { get; set; }
+        public int HitRate { get; set; }
+
+        /* private Texture2D playerTexture;
+         private Texture2D healthTexture; */
+
+        public Player(Game game)
+        {
+
+            Health = 10;
+            MaxHealth = Health;
+            Attack = 1;
+            HitRate = 50;
+
+
+
+        }
+    }
+}
